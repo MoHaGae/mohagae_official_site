db67f796a6f773228434219ce6dc4128be8e9d60
Choi Shin Woo <sinwoo1225@gmail.com>
2020-04-25 22:09:44 +0900

    README.md, index페이지 기능 추가
    readme.md 파일추가 및 index.js초안작성
    index.js : 유저가 스크롤 다운시 header를 small형태로 변환 (css
    transition 적용)

---
+++ README.md
@@ -0,0 +1,49 @@
+# 모하개 공식 웹사이트
+
+## 모하개 공식 사이트 클라이언트(HTML, CSS, JavaScript)
+
+[모하개 사이트 바로가기](https://mohagae.github.io/mohagae_official_site/)  ⬅ 현재 gitgub page 호스팅 추후 netlify에 배포 예정
+
+*❗ 반응형은 지원하지 않습니다.*
+
+---
+
+### ✅ TODO
+
+* 페이지
+  - [ ] 메인컨탠츠
+  - [ ] 달력 
+  - [ ] 게시판
+  
+* 기능
+    - [ ] 로그인 
+    - [ ] 회원가입 
+  
+* 배포
+   - [ ] netlify에 배포 (클라이언트)
+   - [ ] heroku에 배포 (서버)
+   - [ ] 도매인 등록
+
+---
+
+## 개발일지 ⏰📆
+
+**2020년 4월 25일** 
+- `index.html` 초안 작성
+- `index.css` 초안 작성
+- `index.js` 초안 작성
+- `header` 및 `footer` 작성
+
+---
+## 파일설명 💾
+* `index.html` : 메인 페이지 (개발시 copy하여 `main`태그내에 html 작성)
+* `index.css` : `index.html`을 위한 css 파일
+* `reset.css` : 브라우저에 기본적으로 설정되어있는 css설정을 초기화하는 파일
+* `index.js` : `index.html`을 위한 js 파일
+---
+
+## 개발시 유의사항 ❗❗❗
+
+* master 브랜치기 아닌 각자의 branch에서 작업할것
+* 같은 파일을 여러 브런치가 임의 수정하지말것 (충돌방지)
+* 정적리소스(이미지,동영상 등등)은 assets파일에 저장 => 추후 클라우드 스토리지에 배포예정
--- js/index.js
+++ js/index.js
@@ -1,1 +1,59 @@
-console.log("hello");
+/*
+    author : 최신우
+    * 기능
+        - 스크롤을 내렸을때 상단헤터가 작아짐
+*/
+
+// 전역변수 및 상수 선언
+const header = document.querySelector("header");
+const headerTitle = document.querySelector(".header__title");
+
+// 함수선언
+
+// 현재 header가 small형태인지 확인하는 함수
+function isSmallHeader(){
+    if(header.classList.contains("small-header")){
+        return true;
+    }
+    return false;
+}
+
+// header를 large형태로 변경
+function showLargeHeader(){
+    header.classList.remove("small-header");
+    headerTitle.classList.remove("hidden");
+}
+
+// header를 small형태로 변경
+function showSmallHeader(){
+    header.classList.add("small-header");
+    headerTitle.classList.add("hidden");
+}
+
+// 현재 스크롤위치가 최상단인지 확인하는 함수
+function isScrollTop(){
+   if(window.scrollY < 100){
+       return true;
+   } 
+   return false;
+}
+
+// 스크롤 이벤트 발생시 실행하는 함수
+function handleScroll(){
+    if(isScrollTop() && isSmallHeader()){
+            showLargeHeader();
+    }else if(!isScrollTop() && !isSmallHeader()){
+            showSmallHeader();
+    }
+}
+
+// 최초 실행함수, 이벤트등록 및 세팅 초기화함수
+function indexInit(){
+    isScrollPosTop = false;
+    window.addEventListener("scroll", handleScroll);
+}
+
+
+// 초기화함수 실행
+indexInit();
+
